71f933a3b94cd521c080feaa00f71533f84a4480
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-05-27 03:59:39 +0100

    fixed successful payment routing

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import FoodItem, Order, CartItem, Tag
+from .models import FoodItem, Order, CartItem, Tag, Rating
 
 # Register your models here.
 admin.site.register(Tag)
 admin.site.register(FoodItem)
 admin.site.register(Order)
 admin.site.register(CartItem)
+admin.site.register(Rating)
